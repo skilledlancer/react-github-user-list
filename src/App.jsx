a9fd8603af50c8bcff3a53f25abe1efd80185fcd
Manish Kumar Namdeo <mknamdeo@gmail.com>
2023-02-09 19:54:27 +0530

    Github user list application

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import GithubUsers from './components/GithubUsers'
+import './index.css'
+
+function App() {
+  return (
+    <GithubUsers />
+  )
+}
+
+export default App
---
+++ src/components/GithubUsers.jsx
@@ -0,0 +1,37 @@
+const url = 'https://api.github.com/users';
+import {useState, useEffect} from 'react';
+
+const GithubUsers = () => {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const response = await fetch(url)
+                const users = await response.json()
+                setUsers(users)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    },[])
+    return <section>
+        <h3>Github Users</h3>
+        <ul className='users'>
+            {users.map((user)=>{
+                const {id, login, avatar_url, html_url} = user
+                return(
+                    <li key={id}>
+                        <img src={avatar_url} alt={login} />
+                        <div>
+                            <h5>{login}</h5>
+                            <a href={html_url}>profile</a>
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+    </section>;
+};
+export default GithubUsers;
